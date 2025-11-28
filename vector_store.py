2086b3c89bfe6a0ae6478823a5f51f5ed4034038
Your ****** <IGotYourAss@The.Ready>
2025-11-28 11:57:58 +0300

    Merge conflict fix

--- vector_store.py
+++ vector_store.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from langchain_chroma import Chroma
 
 def load_vector_store(collection_name: str, embeddings):
@@ -24,31 +23,4 @@ def load_vector_store(collection_name: str, embeddings):
         print("База данных пуста. Необходимо создать БД при помощи db_creator.py")
         raise ValueError
 
-=======
-from langchain_chroma import Chroma
-
-def load_vector_store(collection_name: str, embeddings):
-    """
-    Загрузки векторной БД. Если она ещё не была создана,
-    её необходимо создать при помощи db_creator.py
-
-    Входные данные:
-        collection_name - имя коллекции в БД
-        embeddings - использованные эмбеддинги при создании БД
-
-    Выходные данные:
-        Векторная БД
-    """
-
-    vector_store = Chroma(
-        collection_name=collection_name,
-        embedding_function=embeddings,
-        persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
-    )
-
-    if len(vector_store.get()['ids']) == 0:
-        print("База данных пуста. Необходимо создать БД при помощи db_creator.py")
-        raise ValueError
-
->>>>>>> 810010fa207fedf499ff4d6641469573a501058c
     return vector_store
